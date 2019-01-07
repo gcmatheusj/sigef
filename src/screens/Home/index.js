8aa5bba9cc4196d38dc17775a197deb589e5a25c
gcmatheusj <goca1377@gmail.com>
2019-01-07 15:00:33 -0300

    novos ajustes de layout

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import { Typography, Paper, Button } from '@material-ui/core';
+import { Typography, Paper, Button, Divider } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 import Add from '@material-ui/icons/Add';
@@ -18,112 +18,195 @@ const Home = props => {
     const { classes } = props
     return (
         <Header title="SIGEF">
-            <Paper className={classes.root}>
-                <Typography className={classes.title} variant="subtitle1">Cadastro das Empresas</Typography>
-                <Grid container spacing={24}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="codigo"
-                            name="codigo"
-                            label="Código"
-                            fullWidth
-                            autoComplete="fname"
-                            variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="data"
-                            name="data"
-                            label="Data"
-                            fullWidth
-                            autoComplete="lname"
-                            variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            id="razaoSocial"
-                            name="razaoSocial"
-                            label="Razão Social"
-                            fullWidth
-                            autoComplete="billing address-line1"
-                            variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            id="nomeFantasia"
-                            name="nomeFantasia"
-                            label="Nome Fantasia"
-                            fullWidth
-                            autoComplete="billing address-line2"
-                            variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="contato"
-                            name="contato"
-                            label="Contato"
-                            fullWidth
-                            autoComplete="billing address-level2"
-                            variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField id="email" name="email" label="E-mail" fullWidth variant="outlined" />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="cpnj"
-                            name="cnpj"
-                            label="CNPJ"
-                            fullWidth
-                            autoComplete="billing postal-code"
-                            variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="inscEstadual"
-                            name="inscEstadual"
-                            label="Insc. Estadual"
-                            fullWidth
-                            autoComplete="billing country"
-                            variant="outlined"
-                        />
-                    </Grid>
-                </Grid>
-                <Grid container justify="flex-start">
-                    <Button className={classes.btn} variant='contained' color='primary'>
-                        <Add />
-                        Novo
+            <Grid container justify="center">
+                <Paper className={classes.root}>
+                    <Typography className={classes.title} variant="subtitle1">Cadastro das Empresas</Typography>
+                    
+                    <Grid container justify="flex-end">
+                        <Button className={classes.btnLocalizar} variant='contained' color='primary'>
+                            <Search className={classes.icon} />
+                            Localizar
                         </Button>
-                    <Button className={classes.btn} variant='contained' color='primary'>
-                        <Search />
-                        Localizar
-                    </Button>
-                    <Button className={classes.btn} variant='contained' color='primary'>
-                        <Save />
-                        Salvar
-                    </Button>
-                    <Button className={classes.btn} variant='contained' color='primary'>
-                        <Delete />
-                        Remover
-                    </Button>
-                    <Button className={classes.btn} variant='contained' color='primary'>
-                        <Cancel />
-                        Cancelar
-                    </Button>
-                </Grid>
-            </Paper>
+                    </Grid>
+                    <Grid style={{marginBottom: 20}} container spacing={24}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="codigo"
+                                name="codigo"
+                                label="Código"
+                                fullWidth
+                                autoComplete="fname"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="data"
+                                name="data"
+                                label="Data"
+                                fullWidth
+                                autoComplete="lname"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="razaoSocial"
+                                name="razaoSocial"
+                                label="Razão Social"
+                                fullWidth
+                                autoComplete="billing address-line1"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="nomeFantasia"
+                                name="nomeFantasia"
+                                label="Nome Fantasia"
+                                fullWidth
+                                autoComplete="billing address-line2"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="contato"
+                                name="contato"
+                                label="Contato"
+                                fullWidth
+                                autoComplete="billing address-level2"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField id="email" name="email" label="E-mail" fullWidth variant="outlined"/>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="cpnj"
+                                name="cnpj"
+                                label="CNPJ"
+                                fullWidth
+                                autoComplete="billing postal-code"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="inscEstadual"
+                                name="inscEstadual"
+                                label="Insc. Estadual"
+                                fullWidth
+                                autoComplete="billing country"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="codigo"
+                                name="codigo"
+                                label="Código"
+                                fullWidth
+                                autoComplete="fname"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="data"
+                                name="data"
+                                label="Data"
+                                fullWidth
+                                autoComplete="lname"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="razaoSocial"
+                                name="razaoSocial"
+                                label="Razão Social"
+                                fullWidth
+                                autoComplete="billing address-line1"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="nomeFantasia"
+                                name="nomeFantasia"
+                                label="Nome Fantasia"
+                                fullWidth
+                                autoComplete="billing address-line2"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="contato"
+                                name="contato"
+                                label="Contato"
+                                fullWidth
+                                autoComplete="billing address-level2"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField id="email" name="email" label="E-mail" fullWidth variant="outlined"/>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="cpnj"
+                                name="cnpj"
+                                label="CNPJ"
+                                fullWidth
+                                autoComplete="billing postal-code"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                
+                                id="inscEstadual"
+                                name="inscEstadual"
+                                label="Insc. Estadual"
+                                fullWidth
+                                autoComplete="billing country"
+                                variant="outlined"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Divider />
+                    <Grid container justify='space-between'>
+                        <Grid>
+                            <Button className={classes.btn} size="small" variant='contained' color='primary'>
+                                <Add className={classes.icon} />
+                                Novo
+                            </Button>
+                            <Button className={classes.btn} size="small" variant='contained' color='primary'>
+                                <Save className={classes.icon} />
+                                Salvar
+                            </Button>
+                        </Grid>
+                        <Button className={[classes.btnRemover]} size="small" variant='contained'>
+                            <Delete className={classes.icon} />
+                            Remover
+                        </Button>
+                    </Grid>
+                </Paper>
+            </Grid>
         </Header>
     )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,16 +1,28 @@
 const styles = theme => ({
     root: {
         padding: 20,
-        marginTop: 24
+        marginTop: 24,
     },
     title: {
         fontSize: 20,
         fontWeight: 'bold',
         marginBottom: 20
     },
+    icon: {
+        marginRight: 5
+    },
     btn: {
         marginTop: 20,
         marginRight: 10,
+        color: '#fff'
+    },
+    btnLocalizar: {
+        marginBottom: 20,
+        color: '#fff'
+    },
+    btnRemover: {
+        backgroundColor: '#F44336',
+        marginTop: 20,
         color: '#fff'
     }
 });
